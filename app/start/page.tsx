fb2b9d3ecbe0eaf8f292435a5bf8edb96f7ada77
jambottle <jaewon.walks@gmail.com>
2024-01-07 20:30:23 +0900

    feat: 동전 던지기 완료 시, 64괘 중 해당하는 괘의 번호 및 이미지 표시

--- app/start/page.tsx
+++ app/start/page.tsx
@@ -1,14 +1,18 @@
 'use client';
 
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { useCallback, useEffect, useMemo } from 'react';
 
 import CoinTossButton from '@/_components/CoinTossButton';
 import { EMPTY_STR } from '@/_constants/empty';
+import useHexagramGenerator from '@/_hooks/useHexagramGenerator';
 import useLineGenerator from '@/_hooks/useLineGenerator';
+import Trigram from '@/_models/Trigram';
 import useQuestionStore from '@/_stores/useQuestionStore';
 
 export default function Start() {
+  // Step 1. 질문 제출 Form을 제출하지 않은 경우, 다른 페이지로 접근 제한
   const { replace } = useRouter();
   const { question } = useQuestionStore();
 
@@ -19,6 +23,7 @@ export default function Start() {
     }
   }, [question, replace]);
 
+  // Step 2. 동전을 3개씩 6번 던지는 과정을 useLineGenerator Hook으로 구현
   const { line: line1, generateLine: genLine1 } = useLineGenerator();
   const { line: line2, generateLine: genLine2 } = useLineGenerator();
   const { line: line3, generateLine: genLine3 } = useLineGenerator();
@@ -41,11 +46,62 @@ export default function Start() {
     genLine6();
   }, [genLine1, genLine2, genLine3, genLine4, genLine5, genLine6]);
 
+  // Step 3. 동전을 3개씩 6번 던진 결괏값을 useHexagramGenerator Hook으로 변환
+  const { hexagram, generateHexagram } = useHexagramGenerator();
+
+  useEffect(() => {
+    if (!isEveryLineGenerated) return;
+
+    const innerTrigram = new Trigram(line1!, line2!, line3!);
+    const outerTrigram = new Trigram(line4!, line5!, line6!);
+
+    generateHexagram(innerTrigram, outerTrigram);
+  }, [
+    generateHexagram,
+    isEveryLineGenerated,
+    line1,
+    line2,
+    line3,
+    line4,
+    line5,
+    line6,
+  ]);
+
+  // 3-1) 64괘 중 해당하는 괘의 번호: 두 자리 숫자 형식의 문자열 (ex. 01, 64 등)
+  const formattedHexagramOrder: string | null = useMemo(() => {
+    if (!hexagram) return null;
+    return String(hexagram.order).padStart(2, '0');
+  }, [hexagram]);
+
+  // 3-2) 64괘 중 해당하는 괘의 이미지
+  const hexagramImage: JSX.Element = useMemo(() => {
+    const visibility: string = formattedHexagramOrder ? 'visible' : 'collapse';
+
+    return (
+      <figure
+        className={`flex flex-col items-center justify-center gap-y-2 ${visibility}`}
+      >
+        <Image
+          alt={`Hexagram-${formattedHexagramOrder}`}
+          src={`https://www.iching-online.com/images/hexagram${formattedHexagramOrder}.png`}
+          width={200}
+          height={224}
+          className="min-h-56"
+        />
+        <figcaption className="min-h-7 text-base font-semibold leading-7 text-gray-900">
+          {formattedHexagramOrder ? `#${formattedHexagramOrder}` : EMPTY_STR}
+        </figcaption>
+      </figure>
+    );
+  }, [formattedHexagramOrder]);
+
   if (question === EMPTY_STR) return;
 
   return (
     <section className="space-y-12">
-      <div className="flex items-center justify-center">
+      <article className="pt-6">{hexagramImage}</article>
+
+      <div className="mt-6 flex items-center justify-center">
         <div className="space-y-4">
           <CoinTossButton label="Coin1" line={line1} generateLine={genLine1} />
           <CoinTossButton label="Coin2" line={line2} generateLine={genLine2} />
--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.iching-online.com',
+        pathname: '**',
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
